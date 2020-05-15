1b425f93f94daee699d559022ef9a0e19625f562
sidaliadbez <hs_zebda@esi.dz>
2020-05-16 00:05:51 +0200

    update Main V1

--- app/src/main/java/com/example/worldairpollution/MainActivity.kt
+++ app/src/main/java/com/example/worldairpollution/MainActivity.kt
@@ -44,8 +44,27 @@ lateinit var option : Spinner
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         }
+val homeFeed = fetchJson1()
+        print("hehe")
+        var blads = arrayListOf<blad>()
+        if ( homeFeed !=null){
 
-        fetchJson()
+        for ( i in 0 until homeFeed?.data?.size!!){
+
+           val api2 =  fetchJson3(homeFeed.data[i].country)
+            val  api3= fetchJson4(api2,homeFeed.data[i].country)
+            if (api2 != null && api3 != null){
+                val blad = blad(homeFeed.data[i].country,api2,api3)
+                if (blad != null){
+                    blads.add(blad)
+                }
+
+            }
+
+        }
+        setupRecyclerView(blads)
+        }
+        print(homeFeed!!.data[0].country)
         var barChart :BarChart = findViewById(R.id.barchart)
         barchart.setDrawBarShadow(false)
         barChart.setDrawValueAboveBar(true)
@@ -62,16 +81,21 @@ lateinit var option : Spinner
         barEntries.add(BarEntry(7f,30f))
         barEntries.add(BarEntry(8f,36f))
         barEntries.add(BarEntry(9f,30f))
-        barEntries.add(BarEntry(10f,47f))
+        barEntries.add(BarEntry(10f,200f))
 
             val  leftAxis : YAxis= barChart.getAxisLeft();
         leftAxis.textColor=Color.WHITE
         leftAxis.axisLineColor=Color.WHITE
-
       val rightAxis :YAxis = barChart.getAxisRight();
     rightAxis.setEnabled(false);
-
+var contry = ArrayList<String>()
+        contry.add("algeria")
+        contry.add("algeria")
+        contry.add("algeria")
+        contry.add("algeria")
+        contry.add("algeria")
     val xAxis:XAxis = barChart.getXAxis();
+
     xAxis.setEnabled(false);
         var barDataSet = BarDataSet(barEntries,"Data Set1")
 
@@ -88,6 +112,9 @@ lateinit var option : Spinner
     }
 
 
+
+
+/*
     fun fetchJson()  {
         val client = OkHttpClient()
         val request = Request.Builder()
@@ -116,7 +143,6 @@ lateinit var option : Spinner
         var blads = arrayListOf<blad>()
         var api2: Api2? = null
         var status1 :Status?= null
-        var status2 :Status?= null
         for (i in 0..homeFeed.data.size - 1){
 
         val client = OkHttpClient()
@@ -138,8 +164,8 @@ lateinit var option : Spinner
                         val gson = GsonBuilder().create()
                         status1 =    gson.fromJson(body, Status::class.java)
                         if (status1?.status.equals("ok")){
-                            println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+homeFeed.data[i].country)
                              api2 =    gson.fromJson(body, Api2::class.java)
+                            println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+homeFeed.data[i].country + api2?.data?.aqi + api2?.data?.time?.s)
                         }
 
 }
@@ -169,20 +195,21 @@ lateinit var option : Spinner
 
                         var body = response.body()?.string()
                         val gson = GsonBuilder().create()
-                       // status2 =    gson.fromJson(body, Status::class.java)
                         if (status1?.status.equals("ok")){
 
                             val api3 =    gson.fromJson(body, Array<Api3>::class.java)
-                            val blad= api2?.let { blad(homeFeed.data[i].country, it,api3) }
+                            val blad = blad(homeFeed.data[i].country,api2!!,api3)
+                            if (homeFeed.data[i].country=="Algeria"){
+                                print("hello")
+                            }
+                            val coco= api2?.let { blad(homeFeed.data[i].country, it,api3) }
                             if (blad != null) {
                                 blads.add(blad)
                             }
-                             runOnUiThread {
-                                 setupRecyclerView(blads)
-                             }
-                        }
 
+                        }
                     }
+
                 }
 
             })
@@ -192,10 +219,17 @@ lateinit var option : Spinner
 
 
     }
-
+        runOnUiThread {
+            println("taille:  "+blads.size)
+            for (blad in blads){
+                println("Le pay: "+blad.country )
+            }
+            blads.sortedBy { it.country }
+            setupRecyclerView(blads)
+        }
 
     }
-
+*/
     private fun setupRecyclerView(blads: ArrayList<blad>) {
         val layoutManager = LinearLayoutManager(this)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
@@ -210,6 +244,103 @@ lateinit var option : Spinner
 
 }
 
+
+fun fetchJson1()  : HomeFeed? {
+    val client = OkHttpClient()
+    var homeFeed :HomeFeed? = null
+    val request = Request.Builder()
+        .url("https://api.airvisual.com/v2/countries?key=7be3b8fb-772d-4a92-ab9b-01690c31aff6")
+        .build()
+
+
+    client.newCall(request).enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {
+            print("sooooooooooooooooorry")
+        }
+
+        override fun onResponse(call: Call, response: Response) {
+            var cool = response.body()?.string()
+            print(cool )
+            val gson = GsonBuilder().create()
+             val homefeed= gson.fromJson(cool,HomeFeed::class.java)
+         //   homeFeed!!.data?= homefeed!!.data?
+                println("hehe")
+
+        }
+
+    })
+    println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+homeFeed!!.data[0]?.country)
+    return homeFeed
+}
+
+fun fetchJson3(name:String): Api2?{
+    var blads = arrayListOf<blad>()
+    var api2: Api2? = null
+    var status1 :Status?= null
+        val client = OkHttpClient()
+        val request = Request.Builder()
+
+            .url("https://api.waqi.info/feed/"+name+"/?token=43e2d6a3b897ab60ac376128719ee9cce0f790bb")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                print("sooooooooooooooooorry")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful){
+
+                    var body = response.body()?.string()
+                    val gson = GsonBuilder().create()
+                    status1 =    gson.fromJson(body, Status::class.java)
+                    if (status1?.status.equals("ok")){
+                        api2 =    gson.fromJson(body, Api2::class.java)
+                        println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+name + api2?.data?.aqi + api2?.data?.time?.s)
+                    }
+
+                }
+            }
+
+        })
+
+return api2
+}
+
+
+
+
+fun fetchJson4(api2: Api2?, name :String) : Array<Api3>? {
+    var api3 : Array<Api3>? =null
+    val client2 = OkHttpClient()
+    val request2 = Request.Builder()
+
+        .url("https://restcountries.eu/rest/v2/name/"+name)
+        .build()
+
+
+    client2.newCall(request2).enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {
+            print("sooooooooooooooooorry")
+        }
+
+        override fun onResponse(call: Call, response: Response) {
+            if (response.isSuccessful){
+                if (api2 != null){
+
+
+
+                var body = response.body()?.string()
+                val gson = GsonBuilder().create()
+                    api3 =    gson.fromJson(body, Array<Api3>::class.java)
+                }
+            }
+
+        }
+
+    })
+return  api3
+}
 class HomeFeed(val data : List<data>): Serializable
 class data (val country :String): Serializable
 class Status(val status:String ): Serializable
